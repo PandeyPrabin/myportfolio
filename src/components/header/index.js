778bb66ab93f3cd0b1501ceac7826e72a9485f5f
Prabin Pandey <prabinpandey95@gmail.com>
2021-12-15 18:07:23 +0200

    menu button added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './header.css'
+import Mobile from './mobile'
+//import Mobile from './mobile'
+import Web from './web'
 
 function Header() {
     return (
-        <div>
-            This is header
+        <div className='header'>
+            <div className='logo'>Prabin</div>
+            <div className='menu'>
+            <div className='web-menu'>
+                <Web />
+            </div>
+            <div className='mobile-menu'>
+                <div>
+
+                    <i class="fi fi-rr-Apps"></i>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 
