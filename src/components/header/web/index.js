2387dc89f355e788796eefe02ad6ecbd4a98def9
Prabin Pandey <prabinpandey95@gmail.com>
2022-05-18 16:59:26 +0300

    Menu Icon image has been added
    Menu Icon image has been added from react icons

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import './web.css'
+import {FaLaptopCode} from 'react-icons/fa'
+import {MdOutlineContactPhone} from 'react-icons/md'
+import {MdWorkOutline} from 'react-icons/md'
+import {HiOutlinePencilAlt} from 'react-icons/hi'
 
 function Web() {
     return <div className='web'>
             <div className='web-option'>
                 <a href='#project'>
-                <i class= 'option-icon'></i>Projects
+                <i class= 'option-icon'></i><HiOutlinePencilAlt />Projects
                 </a>
             </div>
             <div className='web-option'>
                 <a href='#skills'>
-                <i class= 'option-icon'></i>Skills
+                <i class= 'option-icon'></i><FaLaptopCode />Skills
 
                 </a>
             </div>
             <div className='web-option'>
                 <a href='#work'>
-                <i class= 'option-icon'></i>Work
+                <i class= 'option-icon'></i><MdWorkOutline />Work
 
                 </a>
             </div>
             <div className='web-option'>
                 <a href='#contact'>
-                <i class= 'option-icon'></i>Contact
+                <i class= 'option-icon'></i><MdOutlineContactPhone />Contact
 
                 </a>
             </div>
