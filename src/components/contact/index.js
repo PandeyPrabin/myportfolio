7ecfb79079fbb47de635f7f47ea89b96fd1a890c
Prabin Pandey <prabinpandey95@gmail.com>
2022-06-16 04:03:14 +0300

    contact component is created
    In contact component social contact is added

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Separator from './../common/separator'
+import SocialContact from './../common/social-contact'
+
+
+import './contact.css'
 
 function Contact() {
   return (
-    <div>This is Contact</div>
+    <div className='contact'>
+        <Separator />
+        <label className='section-title'>Contact</label>
+        <div className='contact-container'>
+            <div className='contact-left'>
+              <p>Want to get in touch? Contact me on the any of the platforms.</p>
+              <SocialContact />
+            </div>
+            <div className='download'>
+              
+            </div>
+        </div>
+    </div>
   )
 }
 
