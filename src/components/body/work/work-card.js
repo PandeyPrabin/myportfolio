446ca120826de651d5769ecca714122de01dbff9
Prabin Pandey <prabinpandey95@gmail.com>
2022-06-16 03:35:45 +0300

    work card and work data file is working now

---
+++ src/components/body/work/work-card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './work-card.css'
+
+function WorkCard({item}) {
+  return (
+    <div className='work-card'>
+        <img src={item.companyLogo} alt='' className='work-logo'/>
+        <div className='work-info'>
+            <label className='company-logo'>{item.company}</label>
+            <div className='work-dates'>
+                <label>{item.dateJoining}</label>-<label>{item.dateEng}</label>
+            </div>
+            <div className='work-desc'>
+                <p>{item.work}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WorkCard
