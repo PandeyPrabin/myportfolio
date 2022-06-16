c03e8f5b56f7905774a0acc7b38cc5b9f88e3f22
Prabin Pandey <prabinpandey95@gmail.com>
2022-06-16 03:22:26 +0300

    data file for work section is added
    data file created to fetch data instead of backend

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Separator from '../../common/separator'
+
+import './work.css'
 
 function Work() {
   return (
-    <div>This is Work</div>
+    <div className='work'>
+      <Separator />
+      <label className='section-title'>Work</label>
+    </div>
   )
 }
 
